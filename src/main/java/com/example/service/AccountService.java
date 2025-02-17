ad6db6e296b9561b9637b2e4a4b6545a624a4377
Josan Limbu <josanlimbu01@gmail.com>
2025-02-17 07:46:08 +0000

    added annotations to the immediate outter layer

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,9 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
 public class AccountService {
 }
