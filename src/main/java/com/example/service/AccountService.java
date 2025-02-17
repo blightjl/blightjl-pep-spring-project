44ac6b7add1fdda7d0e7c57fc40ffcc3b8ea6dab
Josan Limbu <josanlimbu01@gmail.com>
2025-02-17 19:51:37 +0000

    Place holder method content for services and repositories.

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -3,21 +3,29 @@ package com.example.service;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Account;
+import com.example.repository.AccountRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service
 public class AccountService {
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
     public Account registerAccount(Account account) {
-        return null;
+        return this.accountRepository.save(account);
     }
 
     public Account logIntoAccount(Account account) {
-        return null;
+        return this.accountRepository.save(account);
     }
 
     public boolean accountExists(String username, int account_id, boolean byUsername) {
-        return false;
+        return this.accountRepository.existsById(account_id);
     }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Message;
+import com.example.repository.MessageRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,27 +13,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class MessageService {
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
     public Message addMessage(Message message) {
-        return null;
+        return this.messageRepository.save(message);
     }
 
-    public Optional<List<Message>> getAllMessages() {
-        return Optional.empty();
+    public List<Message> getAllMessages() {
+        return this.messageRepository.findAll();
     }
 
     public Optional<Message> getMessageByID(int message_id) {
-        return Optional.empty();
+        return this.messageRepository.findById(message_id);
     }
 
-    public Optional<Message> updateMessage(int message_id, Message message) {
-        return Optional.empty();
+    public void updateMessage(int message_id, Message message) {
+        Message updatedMessage = message;
+        updatedMessage.setMessageId(message_id);
+        this.messageRepository.save(updatedMessage);
     }
 
-    public Optional<Message> deleteMessage(int message_id) {
-        return Optional.empty();
+    public void deleteMessage(int message_id) {
+        this.messageRepository.deleteById(message_id);
     }
 
-    public Optional<List<Message>> getMessagesByAccountID(int account_id) {
-        return Optional.empty();
+    public List<Message> getMessagesByAccountID(int account_id) {
+        return this.messageRepository.findAllByPostedBy(account_id);
     }
 }
